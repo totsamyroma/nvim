22c427bf2749cb283e0d7988ba84affeec8a2f48
Roma <totsamyroma@gmail.com>
2024-12-08 18:31:04 +0200

    Added Find files and Grep in folder

--- lua/plugins/lsp_zero.lua
+++ lua/plugins/lsp_zero.lua
@@ -2,6 +2,7 @@ return {
 	'VonHeikemen/lsp-zero.nvim', branch = 'v4.x',
 	dependencies = {
 		'neovim/nvim-lspconfig',
+		'hrsh7th/cmp-path',
 		'hrsh7th/cmp-nvim-lsp',
 		'hrsh7th/nvim-cmp',
 	}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,7 @@ return {
 	'nvim-telescope/telescope.nvim', tag = '0.1.8',
 
 	dependencies = {
-		'nvim-lua/plenary.nvim'
+		'nvim-lua/plenary.nvim',
+        'nvim-telescope/telescope-file-browser.nvim',
 	}
 }
